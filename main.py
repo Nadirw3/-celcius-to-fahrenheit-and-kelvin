dc28d6fc02dfc059adfa2add79baa16d83147eff
Nadir Zülfüqarlı <109599790+Nadirw3@users.noreply.github.com>
2022-09-01 13:15:31 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+celcius = int(input("Write Celcius to convert Fahrenheit and Kelvin: "));
+fahrenheit = (celcius * 1.8) + 32
+kelvin = celcius + 273.15
+
+print("fahrenheit is", fahrenheit,"kelvin is", kelvin)
